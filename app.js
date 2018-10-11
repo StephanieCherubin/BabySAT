c40b20194ab41529504d2b3e9a4b95d3bf18158c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-10-11 00:42:08 -0700

    added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-
-app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
-})
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/babySAT');
+const Review = mongoose.model('Review', {
+  title: String,
+  babysitterName: String
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
@@ -16,11 +18,21 @@ app.set('view engine', 'handlebars');
 
 // OUR MOCK ARRAY OF PROJECTS
 let reviews = [
-  { title: "Great Person", babySATterName: "Belinda" },
-  { title: "Awesome Personality", babySATterName: "Tatiana" }
+  { title: "Great Review", babysitterName: "Batman II" },
+  { title: "Awesome Movie", babysitterName: "Titanic" }
 ]
 
 // INDEX
 app.get('/reviews', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
+})
+
+app.get('/', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
