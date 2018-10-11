1c15babd11e6d5e224248e49a9363a78609ef5a0
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-10-11 01:22:56 -0700

    users can see a single review

--- app.js
+++ app.js
@@ -48,10 +48,10 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 
