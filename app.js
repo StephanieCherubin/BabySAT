8110374701c39546fb8e9fc75e57ea045de17e4c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-10-12 00:23:03 -0700

    edit app.js for errors/bugs

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
-const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/babySAT');
 
 const Review = require('./models/review')
 const Babysatter = require("./models/babysatter")
 const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static('./public'))
 app.use(methodOverride('_method'))
@@ -15,5 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var exphbs = require('express-handlebars');
 
+//Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// routes
+// reviewController(app);
+// babysatterController(app);
+
+// Mongoose Connection
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/BabySAT";
+
+mongoose.connect( mongoUri, { useNewUrlParser: true });
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`)
+});
+
+module.exports = app
